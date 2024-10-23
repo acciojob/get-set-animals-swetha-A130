db9d32ac4cd0f6e6318898fca193448bbf7f033f
swetha-A130 <swethachintalapudi6@gmail.com>
2024-10-23 21:52:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species; // Use underscore to denote a private property
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound
+  makeSound() {
+    console.log("The animal makes a sound");
+  }
+}
+
+class Cat extends Animal {
+  // Method to purr
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  // Method to bark
+  bark() {
+    console.log("woof");
+  }
+}
+const myCat = new Cat("Siamese");
+console.log(myCat.species); // Output: Siamese
+myCat.makeSound(); // Output: The animal makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Bulldog");
+console.log(myDog.species); // Output: Bulldog
+myDog.makeSound(); // Output: The animal makes a sound
+myDog.bark(); // Output: woof
+
 
 // Do not change the code below this line
 window.Animal = Animal;
