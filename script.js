0fd0426c73bdbe71c9bf05ce6ad38c1e5feed87f
swetha-A130 <swethachintalapudi6@gmail.com>
2024-10-23 22:02:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,3 +34,8 @@ myCat.purr(); // Output: purr
 const myDog = new Dog("Golden Retriever");
 myDog.makeSound(); // Output: The Golden Retriever makes a sound
 myDog.bark(); // Output: woof
+
+// Do not change the code below this line
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
