5451825b1c4f098d4354c7e0ea4c23087d36b8ae
swetha-A130 <swethachintalapudi6@gmail.com>
2024-10-23 22:04:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -27,13 +27,7 @@ class Dog extends Animal {
     console.log("woof");
   }
 }
-const myCat = new Cat("Siamese");
-myCat.makeSound(); // Output: The Siamese makes a sound
-myCat.purr(); // Output: purr
 
-const myDog = new Dog("Golden Retriever");
-myDog.makeSound(); // Output: The Golden Retriever makes a sound
-myDog.bark(); // Output: woof
 
 // Do not change the code below this line
 window.Animal = Animal;
